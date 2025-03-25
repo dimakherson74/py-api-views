50ea0ead2949156874b0061d97c9a767433a0b23
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-25 15:57:12 +0100

    add model Genre to views.py and urls.py

--- cinema/views.py
+++ cinema/views.py
@@ -1,11 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, generics
 
 from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
 
-from cinema.models import Movie
-from cinema.serializers import MovieSerializer
+from cinema.models import Movie, Genre
+from cinema.serializers import MovieSerializer, GenreSerializer
 
 
 @api_view(["GET", "POST"])
@@ -43,3 +44,37 @@ def movie_detail(request, pk):
     if request.method == "DELETE":
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+class GenreList(APIView):
+    def get(self, request) -> Response:
+        genres = Genre.objects.all()
+        serializer = GenreSerializer(genres, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request) -> Response:
+        serializer = GenreSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class GenreDetail(APIView):
+   def get_object(self, pk: int) -> Genre:
+       return get_object_or_404(Genre, pk=pk)
+
+   def get(self, request, pk: int) -> Response:
+       genre = self.get_object(pk)
+       serializer = GenreSerializer(genre)
+       return Response(serializer.data, status=status.HTTP_200_OK)
+
+   def put(self, request, pk: int) -> Response:
+       genre = self.get_object(pk)
+       serializer = GenreSerializer(genre, data=request.data)
+       if serializer.is_valid():
+           serializer.save()
+           return Response(serializer.data)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+   def delete(self, request, pk: int) -> Response:
+       self.get_object(pk).delete()
+       return Response(status=status.HTTP_204_NO_CONTENT)
