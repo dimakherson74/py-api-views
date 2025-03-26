c762e4ec98b46b4c326d538cfa30b0861b0aaef7
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-26 19:03:05 +0100

    add model Actor to views.py and urls.py

--- cinema/urls.py
+++ cinema/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 
-from cinema.views import movie_list, movie_detail, GenreList, GenreDetail
+from cinema.views import movie_list, movie_detail, GenreList, GenreDetail, ActorList, ActorDetail
 
 urlpatterns = [
     path("movies/", movie_list, name="movie-list"),
     path("movies/<int:pk>/", movie_detail, name="movie-detail"),
     path("genre/", GenreList.as_view(), name="genre-list"),
     path("genre/<int:pk>/", GenreDetail.as_view(), name="genre-detail"),
+    path("actor/", ActorList.as_view(), name="actor-list"),
+    path("actor/<int:pk>/", ActorDetail.as_view(), name="actor-detail"),
 ]
 
 app_name = "cinema"
--- cinema/views.py
+++ cinema/views.py
@@ -1,12 +1,12 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status, generics
+from rest_framework import status, generics, mixins
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre
-from cinema.serializers import MovieSerializer, GenreSerializer
+from cinema.models import Movie, Genre, Actor
+from cinema.serializers import MovieSerializer, GenreSerializer, ActorSerializer
 
 
 @api_view(["GET", "POST"])
@@ -63,12 +63,12 @@ class GenreDetail(APIView):
        return get_object_or_404(Genre, pk=pk)
 
    def get(self, request, pk: int) -> Response:
-       genre = self.get_object(pk)
+       genre = self.get_object(pk=pk)
        serializer = GenreSerializer(genre)
        return Response(serializer.data, status=status.HTTP_200_OK)
 
    def put(self, request, pk: int) -> Response:
-       genre = self.get_object(pk)
+       genre = self.get_object(pk=pk)
        serializer = GenreSerializer(genre, data=request.data)
        if serializer.is_valid():
            serializer.save()
@@ -76,5 +76,40 @@ class GenreDetail(APIView):
        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
    def delete(self, request, pk: int) -> Response:
-       self.get_object(pk).delete()
+       self.get_object(pk=pk).delete()
        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ActorList(
+   mixins.ListModelMixin,
+   mixins.CreateModelMixin,
+   generics.GenericAPIView
+):
+   queryset = Actor.objects.all()
+   serializer_class = ActorSerializer
+
+   def get(self, request, *args, **kwargs):
+       return self.list(request, *args, **kwargs)
+
+   def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
+
+
+class ActorDetail(
+   mixins.RetrieveModelMixin,
+   mixins.UpdateModelMixin,
+   mixins.DestroyModelMixin,
+   generics.GenericAPIView
+):
+   queryset = Actor.objects.all()
+   serializer_class = ActorSerializer
+
+   def get(self, request, *args, **kwargs):
+       return self.retrieve(request, *args, **kwargs)
+
+   def put(self, request, *args, **kwargs):
+       return self.update(request, *args, **kwargs)
+
+   def delete(self, request, *args, **kwargs):
+       return self.destroy(request, *args, **kwargs)
+
