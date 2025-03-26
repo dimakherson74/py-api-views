dbc859e803e0fdbe40e790eeef9515acdb4bae46
Dolgov Dmytro <dimakherson74@gmail.com>
2025-03-26 20:18:59 +0100

    Solution

--- cinema/views.py
+++ cinema/views.py
@@ -61,11 +61,16 @@ class GenreList(APIView):
         serializer = GenreSerializer(genres, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+
     def post(self, request) -> Response:
         serializer = GenreSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+        # serializer.is_valid(raise_exception=True)
+        # serializer.save()
+        # return Response(serializer.data, status=status.HTTP_201_CREATED)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class GenreDetail(APIView):
